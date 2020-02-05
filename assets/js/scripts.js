eeb3bbd475015bd973c2562ffba11584824e8257
Steven Boulom <sboulom@gmail.com>
2020-02-04 18:41:07 -0800

    added initial files with psuedo code

---
+++ assets/js/scripts.js
@@ -0,0 +1,40 @@
+//Use the OpenWeather API to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions.
+// Use AJAX to hook into the API to retrieve data in JSON format.
+// Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+// Display the following under current weather conditions:
+// City
+// Date
+// Icon image (visual representation of weather conditions)
+// Temperature
+// Humidity
+// Wind speed
+// UV index
+// Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city.
+// Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
+// Date
+// Icon image (visual representation of weather conditions)
+// Temperature
+// Humidity
+// !!!!!!!!!!!!!!!!!!-------HINTS-----------!!!!!!!!!!!!!!!
+// Create multiple functions within your application to handle the different parts of the dashboard:
+// Current conditions
+// 5-Day Forecast
+// Search history
+// UV index
+// You will need to make more than one AJAX call.
+// You will need to hardcode some of the parameters in the API's URL. User input will determine some of the other parameters.
+// Use localStorage to store any persistent data.
+
+
+
+
+// 
+// create a jumbotron for the top
+// create column for the seach input field column 4
+// header for "search for city"
+// input field to search user input city with search button
+// append previous city searched
+// create column for 1 day forecast column 8
+// must include - city name, date, (moments JS), icon image, temperature, humidity, wind speed and UV index CHECK CURRENT WEEK FOR ACTIVITY
+// create column for 5 day forecast column 8
+// must include - date, icon image, temperature, humidity
