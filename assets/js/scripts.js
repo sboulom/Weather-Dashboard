1353b43909f3fe2d62344c520df5a0a489738e6e
Steven Boulom <sboulom@gmail.com>
2020-02-08 13:06:46 -0800

    edited java to make dynamic

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,87 @@
+var apiKey = "&appid=7980901759d3b1066013adeb63af74a7";
+var cityEl = document.querySelector(".city");
+var dateEl = document.querySelector(".date");
+var weatherIconEl = document.querySelector(".weather-Icon");
+var tempEl = document.querySelector(".temperature");
+var humidityEl = document.querySelector(".humidity");
+var windSpeedEl = document.querySelector(".windSpeed");
+var uvEl = document.querySelector(".uvIndex");
+
+$("#find-city").on("click", function(e) {
+  e.preventDefault();
+  var cityName = $("#searchCity")
+    .val()
+    .trim();
+
+  console.log(cityName);
+
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + apiKey;
+  console.log(queryURL);
+
+  callAPI(queryURL);
+});
+
+function callAPI(queryURL) {
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response.coord.lon);
+
+    var city = $(".city");
+    var row = $('<div class="row">');
+    var col = $("<div class='col-sm-8'>");
+    var p = $("<p>" + JSON.stringify(response.coord.lon) + "</p>");
+    // var textarea = $("<textarea>");
+    // var button = $("<button>");
+    // button.addClass = "searchCity";
+    // button.text("Search City");
+
+    city.append(row.append(col.append(p)));
+  });
+}
+
+// console.log(displayWeather)
+// cityEl.innerHTML =
+
+// dateEl.innerHTML =
+
+// weatherIconEl.innerHTML =
+//   `<img src="icons/${weather.iconID}.png"/>`
+
+// tempEl.innerHTML =
+
+// humidityEl.innerHTML =
+//  weather.humidity;
+
+// windSpeedEl.innerHTML =
+//   weather.windspeed;
+// uvEl.innerHTML =
+//  weather.uv
+
+// }
+// // cityEl.innerHTML =
+// displayWeather()
+
+// $("#find-City").on("click", function(event){
+
+//     event.preventDefault();
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       })
+
+//       .then(function(response) {
+
+//         console.log (response);
+
+// create column for 1 day forecast column 8
+// must include - city name, date, (moments JS), icon image, temperature, humidity, wind speed and UV index CHECK CURRENT WEEK FOR ACTIVITY
+// create column for 5 day forecast column 8
+// must include - date, icon image, temperature, humidity
+
 //Use the OpenWeather API to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions.
 // Use AJAX to hook into the API to retrieve data in JSON format.
 // Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
@@ -23,18 +107,4 @@
 // UV index
 // You will need to make more than one AJAX call.
 // You will need to hardcode some of the parameters in the API's URL. User input will determine some of the other parameters.
-// Use localStorage to store any persistent data.
-
-
-
-
-// 
-// create a jumbotron for the top
-// create column for the seach input field column 4
-// header for "search for city"
-// input field to search user input city with search button
-// append previous city searched
-// create column for 1 day forecast column 8
-// must include - city name, date, (moments JS), icon image, temperature, humidity, wind speed and UV index CHECK CURRENT WEEK FOR ACTIVITY
-// create column for 5 day forecast column 8
-// must include - date, icon image, temperature, humidity
+// Use localStorage to store any persistent //
