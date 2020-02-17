b300eb70dc601e2fc1b95834dd20bfcf029bbb3e
Steven Boulom <sboulom@gmail.com>
2020-02-16 22:18:02 -0800

    updated forecast and updated 5 day

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,84 +27,51 @@ function callAPI(queryURL) {
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    console.log(response.coord.lon);
+    console.log(response);
+    $(".city").append(response.name);
+    var date = moment(response.dt, "X").format("MM/DD/YYYY");
+    $(".date").append(date);
 
-    var city = $(".city");
-    var row = $('<div class="row">');
-    var col = $("<div class='col-sm-8'>");
-    var p = $("<p>" + JSON.stringify(response.coord.lon) + "</p>");
-    // var textarea = $("<textarea>");
-    // var button = $("<button>");
-    // button.addClass = "searchCity";
-    // button.text("Search City");
+    var iconurl =
+      "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+    $("#iconImage").attr("src", iconurl);
 
-    city.append(row.append(col.append(p)));
+    var temp = (response.main.temp - 273.15) * 1.8 + 32;
+    $(".temperature").append(temp);
+    $(".humidity").append(response.main.humidity);
+    $(".windSpeed").append(response.wind.speed);
+
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/uvi?${apiKey}&lat=${lat}&lon=${lon}`,
+      method: "GET"
+    }).then(function(response2) {
+      console.log(response2);
+      $(".uvIndex").append(response2.value);
+      $.ajax({
+        url: `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${apiKey}`,
+        method: "GET"
+      }).then(function(response3) {
+        console.log(response3);
+        var list = response3.list;
+        var row = $("<div class = 'row'>");
+        for (let index = 0; index < list.length; index++) {
+          if (list[index].dt_txt.indexOf("00:00:00") > -1) {
+            var col = $("<div class = 'col-sm-2'>");
+            var date = moment(list[index].dt, "X").format("MM/DD/YYYY");
+            var iconurl =
+              "http://openweathermap.org/img/w/" +
+              list[index].weather[0].icon +
+              ".png";
+            var img = $("<img>");
+            img.attr("src", iconurl);
+            col.append(date, img);
+            row.append(col);
+          }
+        }
+        $("#forecastFive").append(row);
+      });
+    });
   });
 }
-
-// console.log(displayWeather)
-// cityEl.innerHTML =
-
-// dateEl.innerHTML =
-
-// weatherIconEl.innerHTML =
-//   `<img src="icons/${weather.iconID}.png"/>`
-
-// tempEl.innerHTML =
-
-// humidityEl.innerHTML =
-//  weather.humidity;
-
-// windSpeedEl.innerHTML =
-//   weather.windspeed;
-// uvEl.innerHTML =
-//  weather.uv
-
-// }
-// // cityEl.innerHTML =
-// displayWeather()
-
-// $("#find-City").on("click", function(event){
-
-//     event.preventDefault();
-
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//       })
-
-//       .then(function(response) {
-
-//         console.log (response);
-
-// create column for 1 day forecast column 8
-// must include - city name, date, (moments JS), icon image, temperature, humidity, wind speed and UV index CHECK CURRENT WEEK FOR ACTIVITY
-// create column for 5 day forecast column 8
-// must include - date, icon image, temperature, humidity
-
-//Use the OpenWeather API to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions.
-// Use AJAX to hook into the API to retrieve data in JSON format.
-// Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-// Display the following under current weather conditions:
-// City
-// Date
-// Icon image (visual representation of weather conditions)
-// Temperature
-// Humidity
-// Wind speed
-// UV index
-// Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city.
-// Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
-// Date
-// Icon image (visual representation of weather conditions)
-// Temperature
-// Humidity
-// !!!!!!!!!!!!!!!!!!-------HINTS-----------!!!!!!!!!!!!!!!
-// Create multiple functions within your application to handle the different parts of the dashboard:
-// Current conditions
-// 5-Day Forecast
-// Search history
-// UV index
-// You will need to make more than one AJAX call.
-// You will need to hardcode some of the parameters in the API's URL. User input will determine some of the other parameters.
-// Use localStorage to store any persistent //
